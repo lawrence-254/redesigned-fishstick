4b6ee692c2f826d3428f9da6913a6be3809913b7
lawrence-254 <lodhiambo26@gmail.com>
2024-01-06 21:22:21 +0300

    python code

--- codility/binaryGap.py
+++ codility/binaryGap.py
@@ -3,10 +3,26 @@
 a function that converts a number into binary, and checks for the length binary gap
 a binary gap is th zeros between two ones. if there are two, the largest gap is selected
 """
-#convert the integer to binary
-#initialize variable, one to track  the max gap and other to check current gap
-#loop through ech binary character
-#check if digit is zero and add to the variable representing current gap
-#if the digit is one update the longest gap to current gap
-#reset current gap
-#return max gap at the end of the loop
+def binary_gap(integer):
+    #convert the integer to binary
+    to_binary = bin(integer)[2:]
+
+    #initialize variable, one to track  the max gap and other to check current gap
+    active_gap = 0
+    longest_gap = 0
+
+    #loop through ech binary character
+    for b in to_binary:
+        #check if digit is zero and add to the variable representing current gap
+        if b == '0':
+            active_gap += 1
+        else:
+            #if the digit is one compare current gap update the longest gap
+            longest_gap = max(longest_gap, active_gap)
+            #reset current gap
+            active_gap = 0
+    #return max gap at the end of the loop
+    return longest_gap
+
+
+print(binary_gap(0))
